20a4b234c9c15e5ff4778d0b5b435610fb69d4ff
Ivan Lifanov <letalbark@gmail.com>
2025-10-09 18:50:57 +0700

    add create arcs from routes function, use overload extension to pull values from data structures

--- src/Buses.hs
+++ src/Buses.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
 import Text.Read (readMaybe)
-import Data.List (isPrefixOf, intercalate)
+import Data.List (isPrefixOf, intercalate, concat, concatMap)
 import Data.Maybe (mapMaybe)
 
 type StopID = Int
@@ -44,18 +45,20 @@ data Arc where
     arcStopIDNext :: StopID,
     arcRoutesIDs :: [RouteID]
   } -> Arc
-  deriving (Show, Read)
+  deriving (Read)
+instance Show Arc where
+  show (Arc { arcStopID = sID1, arcStopIDNext = sID2, arcRoutesIDs = rIDs}) = show sID1 ++ "-" ++ show rIDs ++ "->" ++ show sID2
 
 
 getStopByStopID :: [Stop] -> Int -> Stop
-getStopByStopID stops sID = head (filter (\s -> stopID s == sID) stops)
+getStopByStopID stops sID = head (filter (\s -> s.stopID == sID) stops)
 
 getRouteByRouteID :: [Route] -> Int -> Route
-getRouteByRouteID routes rID = head (filter (\r -> routeID r == rID) routes)
+getRouteByRouteID routes rID = head (filter (\r -> r.routeID == rID) routes)
 
 getRoutePathAsString :: [Stop] -> Route -> String
-getRoutePathAsString stops route = "Путь Автобуса " ++ routeName route ++ ":\n"
-  ++ intercalate "\n" [show i ++ ". " ++ show (getStopByStopID stops x) | (i, x) <- zip [1..] (routePath route)]
+getRoutePathAsString stops route = "Путь Автобуса " ++ route.routeName ++ ":\n"
+  ++ intercalate "\n" [show i ++ ". " ++ show (getStopByStopID stops x) | (i, x) <- zip [1..] (route.routePath)]
 
 
 hasDuplicates :: Eq a => [a] -> Bool
@@ -71,9 +74,9 @@ _sanityChecks1 stops routes
 _sanityChecks2 :: [Stop] -> [Route] -> IO ()
 _sanityChecks2 stops routes
   | null invalidStops = return ()
-  | otherwise = error ("Routes contain non-existent stopIDs: " ++ show [(routeID r, sid) | (r, sid) <- invalidStops])
+  | otherwise = error ("Routes contain non-existent stopIDs: " ++ show [(r.routeID, sid) | (r, sid) <- invalidStops])
   where
-    invalidStops = [(r, sid) | r <- routes, sid <- routePath r, sid `notElem` map stopID stops]
+    invalidStops = [(r, sid) | r <- routes, sid <- r.routePath, sid `notElem` map stopID stops]
 
 sanityChecks :: [Stop] -> [Route] -> IO ()
 sanityChecks stops routes = do
@@ -85,18 +88,32 @@ parseLines lines = mapMaybe readMaybe filteredLines
   where
     filteredLines = filter (\l -> not (null l || isPrefixOf "--" l)) lines
 
+_combineArcs :: Arc -> [Arc] -> [Arc]
+_combineArcs arc [] = [arc]
+_combineArcs arc (harc:arcs)
+  | harc.arcStopID == arc.arcStopID && harc.arcStopIDNext == arc.arcStopIDNext = Arc {arcStopID = arc.arcStopID, arcStopIDNext = arc.arcStopIDNext, arcRoutesIDs = harc.arcRoutesIDs ++ arc.arcRoutesIDs} : arcs
+  | otherwise = harc : _combineArcs arc arcs
+
+arcsFromRoutes :: [Route] -> [Arc]
+arcsFromRoutes routes = foldr _combineArcs []
+  (concatMap
+    (\r -> zipWith (\ stopID1 stopID2 -> Arc {arcStopID = stopID1, arcStopIDNext = stopID2, arcRoutesIDs = [r.routeID]}) r.routePath (tail r.routePath))
+    routes
+  )
 
 main :: IO ()
 main = do
 
   stopsFileContents <- readFile "stops.hs"
-  let stopsFileLines = lines stopsFileContents 
+  let stopsFileLines = lines stopsFileContents
   let stops = parseLines stopsFileLines :: [Stop]
 
   routesFileContents <- readFile "routes.hs"
-  let routesFileLines = lines routesFileContents 
+  let routesFileLines = lines routesFileContents
   let routes = parseLines routesFileLines :: [Route]
 
   sanityChecks stops routes
-  putStrLn (getRoutePathAsString stops (getRouteByRouteID routes 7))
+
+  let arcs = arcsFromRoutes routes
+  print arcs
 
