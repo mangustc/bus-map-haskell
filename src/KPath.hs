d8dc62accc6d70f25cb3d28630295543cb6c2062
Ivan Lifanov <letalbark@gmail.com>
2025-10-26 17:23:47 +0700

    add a semi-fast-working-k-path-solution-for-least-transfer-task

--- src/KPath.hs
+++ src/KPath.hs
@@ -40,24 +40,28 @@ findKPathsByLength arcs pathAmount startStopID endStopID = _findKPathsByLength [
 
 appendNext :: [RouteID] -> RouteID -> [RouteID]
 appendNext [] x = [x]
-appendNext xs x
-  | last xs == x = xs
-  | otherwise    = xs ++ [x]
+appendNext rIDs x
+  | head rIDs == x = rIDs
+  | otherwise = x : rIDs
 
-findKPathsByTransfers :: [Arc] -> Int -> StopID -> StopID -> [([StopID], [RouteID])]
-findKPathsByTransfers arcs pathAmount startStopID endStopID = _findKPathsByTransfers [] [startStopID] ([], [startStopID])
+findKPathsByTransfers :: [Arc] -> Int -> StopID -> StopID -> [([RouteID], [StopID])]
+findKPathsByTransfers arcs pathAmount startStopID endStopID = map (\(rIDs, sIDs) -> (reverse rIDs, sIDs)) (_findKPathsByTransfers [] (Set.singleton startStopID) ([], [startStopID]))
   where
-    _findKPathsByTransfers :: [([RouteID], [StopID])] -> [StopID] -> ([RouteID], [StopID]) -> [([RouteID], [StopID])]
+    maximumRouteIDs = length (nub (concatMap arcRoutesIDs arcs))
+    _findKPathsByTransfers :: [([RouteID], [StopID])] -> Set.Set StopID -> ([RouteID], [StopID]) -> [([RouteID], [StopID])]
     _findKPathsByTransfers currentPaths visitedStopIDs (buildPathRouteIDs, buildPath)
+      | buildPathRouteIDsLength > maximumRouteIDs = currentPaths
       | any (\(pRIDs, pSIDs) -> pRIDs == buildPathRouteIDs && length pSIDs <= buildPathLength) currentPaths = currentPaths
       | isCurrentPathsComplete && (buildPathRouteIDsLength > worstPathRouteIDsLength || (buildPathRouteIDsLength == worstPathRouteIDsLength) && (buildPathLength > worstPathLength)) = currentPaths
       | currentStopID == endStopID =
-          -- let newPaths = nubBy (\p1 p2 -> fst p1 == fst p2) ((reverse buildPathRouteIDs, reverse buildPath) : currentPaths)
           let newPaths = ((buildPathRouteIDs, reverse buildPath) : currentPaths)
           in if isCurrentPathsComplete then take pathAmount (sortBy (comparing (length . fst) <> comparing (length . snd)) newPaths) else newPaths
       | otherwise =
-          let nextArcs = filter (\arc -> arc.arcStopID == currentStopID && arc.arcStopIDNext `notElem` visitedStopIDs) arcs
-          in foldl' (\acc nextArc -> foldl (\acc2 nextRID -> _findKPathsByTransfers acc2 (nextArc.arcStopIDNext : visitedStopIDs) (appendNext buildPathRouteIDs nextRID, nextArc.arcStopIDNext : buildPath)) acc nextArc.arcRoutesIDs) currentPaths nextArcs
+          let nextArcs = filter (\arc -> arc.arcStopID == currentStopID && Set.notMember arc.arcStopIDNext visitedStopIDs) arcs
+          in foldl' (\acc nextArc -> foldl' (\acc2 nextRID ->
+            let newVisited = Set.insert nextArc.arcStopIDNext visitedStopIDs
+                newBuildPathRouteIDs = appendNext buildPathRouteIDs nextRID
+            in _findKPathsByTransfers acc2 newVisited (newBuildPathRouteIDs, nextArc.arcStopIDNext : buildPath)) acc nextArc.arcRoutesIDs) currentPaths nextArcs
         where
           buildPathRouteIDsLength = length buildPathRouteIDs
           buildPathLength = length buildPath
