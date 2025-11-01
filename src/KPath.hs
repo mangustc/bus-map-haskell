5e0f5f38e3497053b3f05c02cb711ed2b2b50fec
Ivan Lifanov <letalbark@gmail.com>
2025-11-01 21:46:45 +0700

    solution that is slow in some situations, but works as intended and is always faster than the previous custom method

--- src/KPath.hs
+++ src/KPath.hs
@@ -7,6 +7,7 @@ module KPath
 
 import Structures
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.List (insertBy, intersect, nub)
 import Data.Ord (comparing)
 import Debug.Trace (trace)
@@ -23,7 +24,7 @@ findKPathsByTransfers = findKPaths insertSortedByTransfers
 
 -- каждый раз как новый автобус проходится по остановке записывать его в список sIDsVisitedAmount, если автобусов там один, то максимум может быть pathAmount, если два автобуса, то максимум может быть pathAmount * 2 и так далее
 findKPaths :: (QueueElement -> [QueueElement] -> [QueueElement]) -> Graph -> Int -> StopID -> StopID -> [([RouteID], [StopID])]
-findKPaths insertFunction graph pathAmount startSID endSID = reverse (findKPathsByLength' [QueueElement {qeLengthCost = 0, qeTransferCost = 0, qeCurrentStopID = startSID, qePathRouteIDs = [0], qePathStopIDs = [startSID]}] Map.empty [])
+findKPaths insertFunction graph pathAmount startSID endSID = map (\(rIDs, sIDs) -> (tail rIDs, sIDs)) $ reverse (findKPathsByLength' [QueueElement {qeLengthCost = 0, qeTransferCost = 0, qeCurrentStopID = startSID, qePathRouteIDs = [0], qePathStopIDs = [startSID]}] Map.empty [])
   where
     findKPathsByLength' :: [QueueElement] -> Map.Map StopID Int -> [([RouteID], [StopID])] -> [([RouteID], [StopID])]
     findKPathsByLength' queue sIDsVisitedAmount currentPaths
@@ -33,18 +34,18 @@ findKPaths insertFunction graph pathAmount startSID endSID = reverse (findKPaths
           let currentEntry = head queue
               queue' = tail queue
               currentSIDVisitedAmount = Map.findWithDefault 0 currentEntry.qeCurrentStopID sIDsVisitedAmount
-          in if currentSIDVisitedAmount >= pathAmount
+          in if currentSIDVisitedAmount >= pathAmount * 70 || currentEntry.qeTransferCost > 8 || head currentEntry.qePathStopIDs `elem` tail currentEntry.qePathStopIDs
              then findKPathsByLength' queue' sIDsVisitedAmount currentPaths
              else if currentEntry.qeCurrentStopID == endSID
                   then
                     let newPath = (reverse currentEntry.qePathRouteIDs, reverse currentEntry.qePathStopIDs)
                         isPathPointless = any (\path -> newPath `isPointlessAgainst` path) currentPaths
                         plus = if isPathPointless then 0 else 1
-                        filteredPaths = if isPathPointless then trace (show currentEntry) currentPaths else newPath : filter (\path -> not (path `isPointlessAgainst` newPath)) currentPaths
+                        filteredPaths = if isPathPointless then currentPaths else newPath : filter (\path -> not (path `isPointlessAgainst` newPath)) currentPaths
                         newSIDsVisitedAmount = Map.insert currentEntry.qeCurrentStopID (currentSIDVisitedAmount + plus) sIDsVisitedAmount
                     in findKPathsByLength' queue' newSIDsVisitedAmount filteredPaths
                   else
-                    let nextSIDsWithWeights = Map.findWithDefault [] currentEntry.qeCurrentStopID graph
+                    let nextSIDsWithRIDs = Map.findWithDefault [] currentEntry.qeCurrentStopID graph
                         newEntries = [
                           QueueElement {
                             qeLengthCost = currentEntry.qeLengthCost + 1,
@@ -52,8 +53,7 @@ findKPaths insertFunction graph pathAmount startSID endSID = reverse (findKPaths
                             qeCurrentStopID = nextSID,
                             qePathRouteIDs = nextRID: currentEntry.qePathRouteIDs,
                             qePathStopIDs = nextSID : currentEntry.qePathStopIDs
-                            } | (nextSID, nextRIDs) <- nextSIDsWithWeights, nextRID <- nextRIDs ]
+                            } | (nextSID, nextRIDs) <- nextSIDsWithRIDs, nextRID <- nextRIDs ]
                         newQueue = foldr insertFunction queue' newEntries
                         newSIDsVisitedAmount = Map.insert currentEntry.qeCurrentStopID (currentSIDVisitedAmount + 1) sIDsVisitedAmount
                     in findKPathsByLength' newQueue newSIDsVisitedAmount currentPaths
-
