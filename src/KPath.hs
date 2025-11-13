f3731a509606ca8f8c11211eccdf8a7d9a9cfd23
Ivan Lifanov <letalbark@gmail.com>
2025-11-13 13:57:20 +0700

    check if path is ponitless even before it is processed

--- src/KPath.hs
+++ src/KPath.hs
@@ -48,17 +48,33 @@ queueElementToPath qe = Path {
 
     fullSegs = zipWith (\rID (sID, sIDNext) -> (rID, sID, sIDNext)) qe.qePathRouteIDs (zip qe.qePathStopIDs (tail qe.qePathStopIDs))
 
+cutRepeating :: [RouteID] -> [RouteID]
+cutRepeating [] = []
+cutRepeating [rID] = [rID]
+cutRepeating (rID : rIDs)
+  | head nextCutRepeating == rID = nextCutRepeating
+  | otherwise = rID : nextCutRepeating
+  where
+    nextCutRepeating = cutRepeating rIDs
+
+removeOne :: Eq a => a -> [a] -> [a]
+removeOne _ [] = []
+removeOne x (y:ys) | x == y = ys
+removeOne x (y:ys) = y : removeOne x ys
+
 isPointlessAgainst :: QueueElement -> QueueElement -> Bool
 isPointlessAgainst pointlessQE betterQE =
-  let bRIDs = nub betterQE.qePathRouteIDs
-      pRIDs = nub pointlessQE.qePathRouteIDs
-  in length (bRIDs `intersect` pRIDs) == length bRIDs && pointlessQE.qeLengthCost >= betterQE.qeLengthCost
+  let bRIDs = cutRepeating betterQE.qePathRouteIDs
+      pRIDs = cutRepeating pointlessQE.qePathRouteIDs
+  in null (foldr removeOne bRIDs pRIDs) && pointlessQE.qeLengthCost >= betterQE.qeLengthCost
 
 findEveryKPath :: Graph -> Int -> StopID -> StopID -> [QueueElement]
-findEveryKPath graph pathAmount startSID endSID = if length everyPath == 1 then everyPath else filter (\qe -> nub qe.qePathRouteIDs /= [0]) everyPath
+findEveryKPath graph pathAmount startSID endSID = filter (\qe ->
+  not (any (\qe2 -> not (qe.qePathRouteIDs == qe2.qePathRouteIDs && qe.qePathStopIDs == qe2.qePathStopIDs) && qe `isPointlessAgainst` qe2) maybeWithoutZeroPaths)) maybeWithoutZeroPaths
   where
     everyPath = nubBy (\qe1 qe2 -> qe1.qePathRouteIDs == qe2.qePathRouteIDs && qe1.qePathStopIDs == qe2.qePathStopIDs)
       (findKPaths insertSortedByLength graph pathAmount startSID endSID ++ findKPaths insertSortedByTransfers graph pathAmount startSID endSID)
+    maybeWithoutZeroPaths = if length everyPath == 1 then everyPath else filter (\qe -> nub qe.qePathRouteIDs /= [0]) everyPath
 
 findKPathsByLength :: Graph -> Int -> StopID -> StopID -> [Path]
 findKPathsByLength graph pathAmount startSID endSID = map queueElementToPath
@@ -75,43 +91,46 @@ findKPathsByTransfers graph pathAmount startSID endSID = map queueElementToPath
 findKPaths :: (QueueElement -> [QueueElement] -> [QueueElement]) -> Graph -> Int -> StopID -> StopID -> [QueueElement]
 findKPaths insertFunction graph pathAmount startSID endSID = map
   (\qe -> qe {qePathRouteIDs = tail (reverse qe.qePathRouteIDs), qePathStopIDs = reverse qe.qePathStopIDs})
-    $ reverse (findKPathsByLength' [QueueElement {qeLengthCost = 0, qeTransferCost = 0, qeCurrentStopID = startSID, qePathRouteIDs = [-1], qePathStopIDs = [startSID]}] Map.empty [])
+    $ reverse (findKPaths' [QueueElement {qeLengthCost = 0, qeTransferCost = 0, qeCurrentStopID = startSID, qePathRouteIDs = [-1], qePathStopIDs = [startSID]}] Map.empty [])
   where
     maximumRIDsAmount = length (nub (concatMap snd (concat (Map.elems graph))))
-    findKPathsByLength' :: [QueueElement] -> Map.Map StopID Int -> [QueueElement] -> [QueueElement]
-    findKPathsByLength' queue sIDsVisitedAmount currentPaths
+    findKPaths' :: [QueueElement] -> Map.Map StopID Int -> [QueueElement] -> [QueueElement]
+    findKPaths' queue sIDsVisitedAmount currentPaths
       | length currentPaths >= pathAmount = currentPaths
       | null queue = currentPaths
       | otherwise =
           let currentEntry = head queue
               queue' = tail queue
               currentSIDVisitedAmount = Map.findWithDefault 0 currentEntry.qeCurrentStopID sIDsVisitedAmount
-          in if currentSIDVisitedAmount >= pathAmount * maximumRIDsAmount * 3
-             then findKPathsByLength' queue' sIDsVisitedAmount currentPaths
+          in if currentSIDVisitedAmount >= pathAmount * maximumRIDsAmount * 2
+             then findKPaths' queue' sIDsVisitedAmount currentPaths
              else if currentEntry.qeCurrentStopID == endSID
                   then
                     let isPathPointless = any (\path -> currentEntry `isPointlessAgainst` path) currentPaths
                         plus = if isPathPointless then 0 else 1
                         filteredPaths = if isPathPointless then currentPaths else currentEntry : filter (\path -> not (path `isPointlessAgainst` currentEntry)) currentPaths
                         newSIDsVisitedAmount = Map.insert currentEntry.qeCurrentStopID (currentSIDVisitedAmount + plus) sIDsVisitedAmount
-                    in findKPathsByLength' queue' newSIDsVisitedAmount filteredPaths
+                    in findKPaths' queue' newSIDsVisitedAmount filteredPaths
                   else
                     let nextSIDsWithRIDs = Map.findWithDefault [] currentEntry.qeCurrentStopID graph
                         headRID = head currentEntry.qePathRouteIDs
                         newEntries = [
-                          QueueElement {
-                              qeLengthCost = currentEntry.qeLengthCost + if nextRID == 0 then 4 else 1,
-                              qeTransferCost = nextTransferCost,
-                              qeCurrentStopID = nextSID,
-                              qePathRouteIDs = nextRID : currentEntry.qePathRouteIDs,
-                              qePathStopIDs = nextSID : currentEntry.qePathStopIDs
-                            } | (nextSID, nextRIDs) <- nextSIDsWithRIDs,
-                            nextRID <- nextRIDs,
-                            let nextTransferCost = currentEntry.qeTransferCost + (if headRID == nextRID then 0 else 1),
-                            nextTransferCost <= maximumRIDsAmount &&
-                              (nextRID == 0 || headRID == nextRID || nextRID `notElem` tail currentEntry.qePathRouteIDs) &&
-                              nextSID `notElem` currentEntry.qePathStopIDs ]
+                          qe | (nextSID, nextRIDs) <- nextSIDsWithRIDs,
+                          nextRID <- nextRIDs,
+                          let nextTransferCost = currentEntry.qeTransferCost + (if headRID == nextRID then 0 else 1),
+                          let qe = QueueElement {
+                            qeLengthCost = currentEntry.qeLengthCost + if nextRID == 0 then 4 else 1,
+                            qeTransferCost = nextTransferCost,
+                            qeCurrentStopID = nextSID,
+                            qePathRouteIDs = nextRID : currentEntry.qePathRouteIDs,
+                            qePathStopIDs = nextSID : currentEntry.qePathStopIDs
+                          },
+                          nextTransferCost <= maximumRIDsAmount &&
+                            (nextRID == 0 || headRID == nextRID || nextRID `notElem` tail currentEntry.qePathRouteIDs) &&
+                            nextSID `notElem` currentEntry.qePathStopIDs &&
+                            not (any (\path -> qe `isPointlessAgainst` path) currentPaths)
+                          ]
                         newQueue = foldr insertFunction queue' newEntries
                         newSIDsVisitedAmount = Map.insert currentEntry.qeCurrentStopID (currentSIDVisitedAmount + 1) sIDsVisitedAmount
-                    in findKPathsByLength' newQueue newSIDsVisitedAmount currentPaths
+                    in findKPaths' newQueue newSIDsVisitedAmount currentPaths
 
